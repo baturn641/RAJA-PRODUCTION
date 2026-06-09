00be3a51c42045a1c213f994db73d955682fe29c
baturn641 <baturn641@gmail.com>
2026-06-09 19:42:30 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'raja-production-v1';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// Install Service Worker
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    }).then(() => self.skipWaiting())
+  );
+});
+
+// Activate Service Worker
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch Service Worker (Syarat Wajib Chrome Android)
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cachedResponse => {
+      return cachedResponse || fetch(e.request);
+    })
+  );
+});
